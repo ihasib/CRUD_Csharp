e7842c29274a4ea7bd34492f25eb0273b6c67271
Hasib <hasib.jhd@gmail.com>
2019-01-01 13:12:46 +0600

    orderby clause checked

--- Level2_DatabaseByCsharp/Program.cs
+++ Level2_DatabaseByCsharp/Program.cs
@@ -17,15 +17,22 @@ namespace Level2_DatabaseByCsharp
 
             VirtualTraineesDBEntities db = new VirtualTraineesDBEntities();
             //var deptSet = db.Departments;
-            var traineeSet = db.Trainees;
-            //var traineeCSESet = traineeSet.Where(x=>x.DeptId=="3"); //var and Iqueryble both work
-            IQueryable<Trainee> traineeCSESet = traineeSet.Where(x => x.DeptId == "3");
             //List<Department> deptList = deptSet.ToList();
-            List<Trainee> traineeList = traineeCSESet.ToList();
 
-            foreach (Trainee dIndex in traineeList)
+            //var traineeSet = db.Trainees;
+            //var traineeCSESet = traineeSet.Where(x=>x.DeptId=="3"); //var and Iqueryble both work
+            //IQueryable<Trainee> traineeCSESet = traineeSet.Where(x => x.DeptId == "3");
+
+            var v = 3;
+            var dbSet = db.Enrollments;
+            //IQueryable<Enrollment> enrollSet = dbSet.Where(x => x.CourseID > v);
+            IQueryable<Enrollment> enrollSet = dbSet.OrderBy(x => x.TraineeID);
+            List<Enrollment> enrollList = enrollSet.ToList();
+
+
+            foreach (Enrollment dIndex in enrollList)
             {
-                Console.WriteLine(dIndex.Name);
+                Console.WriteLine(dIndex.TraineeID);
             }
             Console.Read();
         }
