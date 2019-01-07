48405113ee0ea669083c620ce7aaf5ec4c021e39
Hasib <hasib.jhd@gmail.com>
2019-01-07 14:08:10 +0600

    EntryAt's datatype is shifted from string to DataTime

--- Level2_DatabaseByCsharp/Program.cs
+++ Level2_DatabaseByCsharp/Program.cs
@@ -16,8 +16,12 @@ namespace Level2_DatabaseByCsharp
             //display data on the console display
 
             VirtualTraineesDBEntities db = new VirtualTraineesDBEntities();
-            //var deptSet = db.Departments;
-            //List<Department> deptList = deptSet.ToList();
+            var deptSet = db.Departments;
+
+            DateTime date = new DateTime(1964, 1, 1);
+            IQueryable<Department> deptBirth = deptSet.Where(x => x.EntryAt > date);
+
+            List<Department> deptList = deptBirth.ToList();
 
             //var traineeSet = db.Trainees;
             //var traineeCSESet = traineeSet.Where(x=>x.DeptId=="3"); //var and Iqueryble both work
@@ -26,13 +30,13 @@ namespace Level2_DatabaseByCsharp
             var v = 3;
             var dbSet = db.Enrollments;
             //IQueryable<Enrollment> enrollSet = dbSet.Where(x => x.CourseID > v);
-            IQueryable<Enrollment> enrollSet = dbSet.OrderBy(x => x.TraineeID);
-            List<Enrollment> enrollList = enrollSet.ToList();
+            //IQueryable<Enrollment> enrollSet = dbSet.OrderBy(x => x.TraineeID);
+            //List<Enrollment> enrollList = enrollSet.ToList();
 
 
-            foreach (Enrollment dIndex in enrollList)
+            foreach (Department dIndex in deptList)
             {
-                Console.WriteLine(dIndex.TraineeID);
+                Console.WriteLine(dIndex.Id);
             }
             Console.Read();
         }
